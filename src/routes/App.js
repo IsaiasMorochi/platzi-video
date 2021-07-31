2f572672fb247251c036953a6c3f69ae64087f91
IsaiasMorochi <isaiasmorochi95@gmail.com>
2021-07-31 19:50:14 -0400

    Container Register

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, NavLink } from 'react-router-dom'
 import Home from "../containers/Home";
 import Login from '../containers/Login';
+import Register from '../containers/Register';
 
 const App = () => (
   <BrowserRouter>
     <Route exact path="/" component={Home} />
     <Route path="/login" component={Login} />
+    <Route path="/register" component={Register} />
   </BrowserRouter>
 );
 
