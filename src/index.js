7c184bdb450a05a2cae2d32690e0dfcae9e2cf6b
IsaiasMorochi <isaiasmorochi95@gmail.com>
2021-08-22 19:26:07 -0400

    Instalación de Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './routes/App';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={createStore(reducer)}>
+    <App />
+  </Provider>, document.getElementById('app'));
